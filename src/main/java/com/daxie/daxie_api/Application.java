cbb42175a2b6b384d858fb59983dd50dbe4874cd
alwaysacc <alwaysacc@163.com>
2019-02-27 17:31:52 +0800

    druid数据源配置，log日志输出。 redis配置 还没有测试是否可用

--- src/main/java/com/daxie/daxie_api/Application.java
+++ src/main/java/com/daxie/daxie_api/Application.java
@@ -1,13 +1,32 @@
 package com.daxie.daxie_api;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+    @Value("${http.port}")
+    private Integer port;
 
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
+        return tomcat;
+    }
+    // 配置http
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(port);
+        return connector;
+    }
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
